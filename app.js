ea850740ac40086b3829b574a1b5cda0c4cd83c9
faqihahfajrianij <faqihahfajriani.j@gmail.com>
2023-11-05 12:06:51 +0800

    New github actions

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const {PORT = 3000} = process.env;
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    return res.json({
+        status: true,
+        message: 'hello',
+        error: null,
+        data: null
+    });
+})
 const artRouter = require('./routes/art.routes');
 app.use('/api/v1/art', artRouter);
 
